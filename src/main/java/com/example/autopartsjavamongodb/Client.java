f76e6bbdc5fb71226d7dd9e716a53134ed908420
Gjonathan252 <g.jonathan252@gmail.com>
2021-09-27 18:11:02 -0700

    Cleaned up main class to implement only methods
    Moved main class search methods to the class Client in order to clean up the code. Also Now to initialize the Mongo connection. The server implementations are now done using the Client java file. This way the main class does not class a mongodb init method and can do it all through 1 call method. More information of the client class - It now includes call methods to the SearchDB class instead of creating its own methods in main

--- src/main/java/com/example/autopartsjavamongodb/Client.java
+++ src/main/java/com/example/autopartsjavamongodb/Client.java
@@ -1,16 +1,13 @@
 package com.example.autopartsjavamongodb;
 
+import com.mongodb.client.*;
+import org.bson.Document;
+
+import static com.mongodb.client.model.Filters.eq;
+
 public class  Client {
     private final static String Serverid = "mongodb+srv://gjonathan:123@cluster0.nceor.mongodb.net/InventoryDB?retryWrites=true&w=majority";
     private final static String DatabaseName = "InventoryDB";
-    public Client()
-    {
-    }
-    //    public Client(String server, String DBN)
-//    {
-//        Serverid = server;
-//        DatabaseName = DBN;
-//    }
     public String getServerid()
     {
         return Serverid;
@@ -19,4 +16,58 @@ public class  Client {
     {
         return DatabaseName;
     }
+    MongoClient client = MongoClients.create(getServerid());
+    MongoDatabase db = client.getDatabase(getDatabaseName());
+
+    public Client()
+    {
+        MongoClient client = MongoClients.create(getServerid());
+        MongoDatabase db = client.getDatabase(getDatabaseName());
+    }
+    public void search(){
+        while (true) {
+            System.out.println("\nWelcome to AutoParts Database \nSelect from the following choices.");
+            SearchDB searchDB = new SearchDB(); //Creating Constructor.
+            searchDB.createSelection(); //Making Selection
+            if (searchDB.isSearchORadd()) //bool check = true. Running option 1. Searching parts in DB
+            {
+                searchDB.createSearch();
+                MongoCollection<Document> SS = db.getCollection(searchDB.getsNum());// Gets store number
+                if (searchDB.getInputCheck().equals("1"))
+                {
+                    try (MongoCursor<Document> cursor = SS.find().iterator()) {
+                        while (cursor.hasNext()) {
+                            System.out.println(cursor.next().toJson());
+                        }
+                    }
+                }
+                else if (searchDB.getInputCheck().equals("2")){
+                    try (MongoCursor<Document> cursor = SS.find(eq("Make", searchDB.getsMake())).iterator()) {
+                        while (cursor.hasNext()) {
+                            System.out.println(cursor.next().toJson());
+                        }
+                    }
+                }
+                else if (searchDB.getInputCheck().equals("3"))
+                {
+                    Document document = searchDB.searchDocAppend();
+                    try (MongoCursor<Document> cursor = SS.find(document).iterator()) {
+                        while (cursor.hasNext()) {
+                            System.out.println(cursor.next().toJson());
+                        }
+                    }
+                }
+                System.out.println("Document successfully searched");
+            }
+            else if (!searchDB.isSearchORadd()) //bool check = false. Running option 2. Adding part
+            {
+                searchDB.createTable(); //Collects Information for adding new part
+                MongoCollection<Document> col = db.getCollection(searchDB.getStoreNum()); //Sets collection name.
+                Document document = new Document(searchDB.DocAppend()); //Creates Doc in constructor. Appends all information.
+                col.insertOne(document); //Pushes document to database.
+                System.out.println("Document inserted successfully");
+            }
+        }
+    }
+
 }
--- src/main/java/com/example/autopartsjavamongodb/main.java
+++ src/main/java/com/example/autopartsjavamongodb/main.java
@@ -9,54 +9,8 @@ import com.mongodb.client.MongoCursor;
 import static com.mongodb.client.model.Filters.*;
 
 public class main {
-    public static void main(String[] args){
-        System.out.println("hello world");
+    public static void main(String[] args) {
         Client server = new Client();
-        MongoClient client = MongoClients.create(server.getServerid());
-        MongoDatabase db = client.getDatabase(server.getDatabaseName());
-
-        while (true) {
-            System.out.println("Welcome to AutoParts Database \nSelect from the following choices.");
-            SearchDB searchDB = new SearchDB(); //Creating Constructor.
-            searchDB.createSelection(); //Making Selection
-            if (searchDB.isSearchORadd()) //bool check = true. Running option 1. Searching parts in DB
-            {
-                searchDB.createSearch();
-                MongoCollection<Document> SS = db.getCollection(searchDB.getsNum());// Gets store number
-                if (searchDB.getInputCheck().equals("1"))
-                {
-                    try (MongoCursor<Document> cursor = SS.find().iterator()) {
-                        while (cursor.hasNext()) {
-                            System.out.println(cursor.next().toJson());
-                        }
-                    }
-                }
-                else if (searchDB.getInputCheck().equals("2")){
-                    try (MongoCursor<Document> cursor = SS.find(eq("Make", searchDB.getsMake())).iterator()) {
-                        while (cursor.hasNext()) {
-                            System.out.println(cursor.next().toJson());
-                        }
-                    }
-                }
-                else if (searchDB.getInputCheck().equals("3"))
-                {
-                    Document document = searchDB.searchDocAppend();
-                    try (MongoCursor<Document> cursor = SS.find(document).iterator()) {
-                        while (cursor.hasNext()) {
-                            System.out.println(cursor.next().toJson());
-                        }
-                    }
-                }
-                System.out.println("Document successfully searched");
-            }
-            else if (!searchDB.isSearchORadd()) //bool check = false. Running option 2. Adding part
-            {
-                searchDB.createTable(); //Collects Information for adding new part
-                MongoCollection<Document> col = db.getCollection(searchDB.getStoreNum()); //Sets collection name.
-                Document document = new Document(searchDB.DocAppend()); //Creates Doc in constructor. Appends all information.
-                col.insertOne(document); //Pushes document to database.
-                System.out.println("Document inserted successfully");
-            }
-        }
+        server.search();
     }
 }
